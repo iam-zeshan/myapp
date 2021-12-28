f24971f8895b2b24a7c6736e1bd22beed4aa80b4
Zeshan Haider <zeshan.haider.turi@gmail.com>
2021-12-28 16:39:35 -0700

    Updates

---
+++ precache-manifest.e330c90d780e7f5d7b3ad47e22f478b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba2e980390ec68ba2e8e",
+    "url": "/myapp/static/css/main.534384bc.chunk.css"
+  },
+  {
+    "revision": "ba2e980390ec68ba2e8e",
+    "url": "/myapp/static/js/main.ba2e9803.chunk.js"
+  },
+  {
+    "revision": "dbf16a7a1f5d12f53526",
+    "url": "/myapp/static/js/1.dbf16a7a.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/myapp/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "a03f1495d5ef8a33c8e4",
+    "url": "/myapp/static/js/runtime~main.a03f1495.js"
+  },
+  {
+    "revision": "946d1efa14490b87a9f9b909be1f248f",
+    "url": "/myapp/index.html"
+  }
+];
